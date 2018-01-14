b4486376b4b95a477d1374620b2d48f988f75f6b
yaoxc06@gmail.com <yaoxc06@gmail.com>
2018-01-14 15:34:56 +0800

    Remove: pre-render

--- server.js
+++ server.js
@@ -12,63 +12,72 @@ var serveStatic = require('serve-static')
 
 app.use(compression())
 
-apiRoutes.get('/getDisc', function (req, res) {
+apiRoutes.get('/getDisc', function(req, res) {
   const url = 'http://ustbhuangyi.com/music/api/getCdInfo'
-  
-  axios.get(url, {
-    params: req.query
-  }).then((response) => {
-    res.json(response.data)
-  }).catch((e) => {
-    console.log(e)
-  })
+
+  axios
+    .get(url, {
+      params: req.query
+    })
+    .then(response => {
+      res.json(response.data)
+    })
+    .catch(e => {
+      console.log(e)
+    })
 })
-apiRoutes.get('/getMusicList', function (req, res) {
+apiRoutes.get('/getMusicList', function(req, res) {
   var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
-  axios.get(url, {
-    headers: {
-      referer: 'https://c.y.qq.com/',
-      host: 'c.y.qq.com'
-    },
-    params: req.query
-  }).then((response) => {
-    res.json(response.data)
-  }).catch((e) => {
-    console.log(e)
-  })
+  axios
+    .get(url, {
+      headers: {
+        referer: 'https://c.y.qq.com/',
+        host: 'c.y.qq.com'
+      },
+      params: req.query
+    })
+    .then(response => {
+      res.json(response.data)
+    })
+    .catch(e => {
+      console.log(e)
+    })
 })
 
-apiRoutes.get('/lyric', function (req, res) {
+apiRoutes.get('/lyric', function(req, res) {
   var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
 
-  axios.get(url, {
-    headers: {
-      referer: 'https://c.y.qq.com/',
-      host: 'c.y.qq.com'
-    },
-    params: req.query
-  }).then((response) => {
-    var ret = response.data
-    if (typeof ret === 'string') {
-      var reg = /^\w+\(({[^\(\)]+})\)$/
-      var matches = response.data.match(reg)
-      if (matches) {
-        ret = JSON.parse(matches[1])
+  axios
+    .get(url, {
+      headers: {
+        referer: 'https://c.y.qq.com/',
+        host: 'c.y.qq.com'
+      },
+      params: req.query
+    })
+    .then(response => {
+      var ret = response.data
+      if (typeof ret === 'string') {
+        var reg = /^\w+\(({[^\(\)]+})\)$/
+        var matches = response.data.match(reg)
+        if (matches) {
+          ret = JSON.parse(matches[1])
+        }
       }
-    }
-    res.json(ret)
-  }).catch((e) => {
-    console.log(e)
-  })
+      res.json(ret)
+    })
+    .catch(e => {
+      console.log(e)
+    })
 })
 
 app.use('/api', apiRoutes)
 
 var oneYear = 60 * 1000 * 60 * 24 * 365
 
-app.use(express.static('./music',  { maxAge: oneYear }))
+app.use(express.static('./music', { maxAge: oneYear }))
 
-module.exports = app.listen(port, function (err) {
+module.exports = app.listen(port, function(err) {
   if (err) {
     console.log(err)
     return

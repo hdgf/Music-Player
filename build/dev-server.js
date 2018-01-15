f910d5b5b5042d8c8c4d8b888ffbb3f4b4d02ed8
forzeny <4661042@qq.com>
2018-01-15 10:55:17 +0800

    Build and modify

--- build/dev-server.js
+++ build/dev-server.js
@@ -25,63 +25,59 @@ var compiler = webpack(webpackConfig)
 var axios = require('axios')
 var apiRouters = express.Router()
 
-apiRouters.get('/getDisc', function(req, res) {
+apiRouters.get('/getDisc', function (req, res) {
   const url = 'http://ustbhuangyi.com/music/api/getCdInfo'
-  axios
-    .get(url, {
-      // headers: {
-      //   referer: 'https://c.y.qq.com/',
-      //   host: 'c.y.qq.com'
-      // },
-      params: req.query
-    })
-    .then(response => {
-      res.json(response.data)
-    })
-    .catch(e => {
-      console.log(e)
-    })
+  
+  axios.get(url, {
+    // headers: {
+    //   referer: 'https://c.y.qq.com/',
+    //   host: 'c.y.qq.com'
+    // },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
 })
 
-apiRouters.get('/getMusicList', function(req, res) {
+apiRouters.get('/getMusicList', function (req, res) {
   var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
-  axios
-    .get(url, {
-      headers: {
-        referer: 'https://c.y.qq.com/',
-        host: 'c.y.qq.com'
-      },
-      params: req.query
-    })
-    .then(response => {
-      res.json(response.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  })
+  .then((response) => {
+    res.json(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 })
-apiRouters.get('/lyric', function(req, res) {
+apiRouters.get('/lyric', function (req, res) {
   var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
-  axios
-    .get(url, {
-      headers: {
-        referer: 'https://c.y.qq.com/',
-        host: 'c.y.qq.com'
-      },
-      params: req.query
-    })
-    .then(response => {
-      var ret = response.data
-      if (typeof ret === 'string') {
-        var reg = /{(.*)}/g
-        var match = ret.match(reg)
-        ret = JSON.parse(match[0])
-      }
-      res.json(ret)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  })
+  .then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /{(.*)}/g
+      var match = ret.match(reg)
+      ret = JSON.parse(match[0])
+    }
+    res.json(ret)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 })
 app.use('/api', apiRouters)
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -94,8 +90,8 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   heartbeat: 2000
 })
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function(compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({
       action: 'reload'
     })
@@ -104,7 +100,7 @@ compiler.plugin('compilation', function(compilation) {
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function(context) {
+Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
     options = {
@@ -125,10 +121,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.posix.join(
-  config.dev.assetsPublicPath,
-  config.dev.assetsSubDirectory
-)
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
 var uri = 'http://localhost:' + port
